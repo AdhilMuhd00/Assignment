a6e14c44e8b6b60538039d6bb9e883b128bd8ff4
Adhil Muhammed <adhilmuhd00@gmail.com>
2024-09-08 22:51:35 +0530

    Add files via upload

---
+++ product_app/src/module/Home.jsx
@@ -0,0 +1,55 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+const Home = () => {
+  var [user, setUser]=useState([])
+  useEffect(()=>{
+  axios.get("https://fakestoreapi.com/products")
+    .then((res)=>{
+      console.log(res.data)
+      setUser(res.data)
+    })
+    })
+  
+
+  return (
+    <div>
+      <br /><h1>Product View</h1>
+    <TableContainer component={Paper}>
+    <Table  sx={{ minWidth: 700 }} aria-label="customized table">
+    <TableHead>
+      <TableRow>
+      
+        <TableCell>Title</TableCell>
+        <TableCell>Image</TableCell>
+        <TableCell>Category</TableCell>
+        <TableCell>Price</TableCell>
+        
+        
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {user.map((products)=>{
+        return(
+
+      
+      <TableRow>
+        
+      <TableCell>{products.title}</TableCell>
+      <TableCell><img src={products.image} alt={products.title} width="50" /></TableCell>
+        <TableCell>{products.category}</TableCell>
+        <TableCell>{products.price}</TableCell>
+        
+        </TableRow>
+        )
+        })}
+    </TableBody>
+    </Table>
+    </TableContainer>
+    </div>
+          
+  )
+}
+
+export default Home
